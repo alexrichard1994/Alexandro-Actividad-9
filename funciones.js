34915b1c315de650c2ef9a50d7819e5ad6897bb4
alexrichard1994 <alexandrorichard@hotmail.com>
2020-11-11 19:19:18 -0500

    commit final

---
+++ funciones.js
@@ -0,0 +1,17 @@
+console.log("primer practica");
+
+var variable1 = "papas";
+
+let variable2 = "fritas";
+
+const variable3 = "kecthup";
+
+console.log (variable1);
+
+console.log (variable2);
+
+console.log (variable3);
+
+var variable1 = "papas";
+
+variable3 = "arroz";
